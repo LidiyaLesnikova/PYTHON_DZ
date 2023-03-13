162b8e99cd73727bc16a51dbd7ffd0c2068c28b8
Lidiya <lesnikoval@yandex.ru>
2023-03-14 00:46:21 +0300

    Задача 12

--- DZ2/task12.py
+++ DZ2/task12.py
@@ -5,3 +5,21 @@
 4 4 -> 2 2
 5 6 -> 2 3
 """
+
+while True:
+    try:
+        S = int(input('введите сумму загаданных чисел: '))
+        P = int(input('введите произведение загаданных чисел: '))
+        flag = True
+        for x in range(P):
+            for y in range(P):
+                if x+y == S and x*y == P:
+                    print(f'x = {x}, y = {y}')
+                    flag = False
+                    break
+        if flag:
+            print('таких чисел не существует, введите заново')
+        else:
+            break       
+    except:
+        print('ошибка ввода типа данных, введите заново')
