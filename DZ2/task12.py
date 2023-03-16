7cab86d714353a0e2604f1211899cf8193b2fe96
Lidiya <lesnikoval@yandex.ru>
2023-03-16 22:43:07 +0300

    Задача 12

--- DZ2/task12.py
+++ DZ2/task12.py
@@ -5,21 +5,42 @@
 4 4 -> 2 2
 5 6 -> 2 3
 """
+import math
 
 while True:
     try:
-        S = int(input('введите сумму загаданных чисел: '))
-        P = int(input('введите произведение загаданных чисел: '))
+        S = int(input('введите сумму загаданных чисел до 1000: '))
+        P = int(input('введите произведение загаданных чисел до 1000: '))
         flag = True
+
+        # через цикл
         for x in range(S):
             for y in range(S):
                 if x+y == S and x*y == P:
-                    print(f'x = {x}, y = {y}')
+                    print(f'1) x = {x}, y = {y}')
                     flag = False
                     break
+            # через условие
+            if x*(S-x) == P:
+                print(f'2) x = {x}, y = {S-x}')
+                flag = False
+        
+        # '''
+        # Через дискриминант квадратного уравнения: - НЕ РАБОТАЕТ!!!! (ВЫВОДИТ НЕВЕРНЫЕ ЗНАЧЕНИЯ)
+        # x + y = S
+        # x * y = P
+        # (S — y) * y = P
+        # y^2 — y * S + P = 0
+        # '''
+        # discriminant = int(S) * int(S) - 4 * int(P)
+        # x1 = int((int(S) + int(math.sqrt(discriminant))) / 2)
+        # y1 = int(S) - x1
+        # print(f"3) {S} {P} -> {str(x1)} {str(y1)}")
+
         if flag:
             print('таких чисел не существует, введите заново')
         else:
-            break       
+            break            
     except:
         print('ошибка ввода типа данных, введите заново')
+
