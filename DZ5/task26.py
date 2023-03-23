fd08d03b1b37f9f4a24d55e658e0dec90e7e4367
Lidiya <lesnikoval@yandex.ru>
2023-03-23 23:52:58 +0300

    Задача 26

--- DZ5/task26.py
+++ DZ5/task26.py
@@ -3,3 +3,18 @@
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8 
 '''
+def degree(a, b):    
+    if b == 0: return 1 
+    return a*degree(a, b-1)
+
+while True:
+    try:
+        A = int(input('Введите число А '))
+        B = int(input('Введите число B '))
+        if B>0:
+            print(f'{A} в степени {B} -> {degree(A, B)}')    
+        else:
+            print(f'{A} в степени {B} -> {1/degree(A, -B)}')   
+        break         
+    except:
+        print('ошибка ввода типа данных, введите заново')
